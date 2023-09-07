aa46277c465b0857c616762e7dac9f52aedd4004
Sihle Calana <CLNSIH001@myuct.ac.za>
2023-09-07 04:13:30 +0200

    final list of countries

--- IP_hostReader.py
+++ IP_hostReader.py
@@ -4,10 +4,10 @@ import os
 countryReader = geoip2.database.Reader('GeoLite2-Country.mmdb')
 asnReader = geoip2.database.Reader('GeoLite2-ASN.mmdb')
 
-university_sites = ["uct.ac.za", "wits.ac.za", "www.sun.ac.za", "uem.mz", "up.ac.mz",
-                    "ucm.ac.mz", "unam.edu.na", "ium.edu.na", "www.nust.na", "udsm.ac.tz",
-                    "aru.ac.tz", "sua.ac.tz", "univ-tlemcen.dz", "umc.edu.dz", "www.univ-alger.dz",
-                    "www.univ-ndere.cm", "www.univ-maroua.cm", "uy1.uninet.cm", "www.ucad.sn" , "ugb.sn" , "uadb.edu.sn"]
+university_sites = ["uct.ac.za", "wits.ac.za", "www.sun.ac.za", "mzuni.ac.mw", "unima.ac.mw",
+                    "unam.edu.na", "ium.edu.na", "www.nust.na", "udsm.ac.tz", "aru.ac.tz",
+                    "sua.ac.tz", "univh2c.ma", "uca.ma", "www.ump.ma", "www.univ-ndere.cm",
+                    "www.univ-maroua.cm", "uy1.uninet.cm","www.ucad.sn", "ugb.sn", "uadb.edu.sn"]
 
 uniData = {}
 
